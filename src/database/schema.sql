8b150389d6c9e13af1334f78440c39a11ee83b3a
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-06 08:40:41 -0300

    Update schema.sql

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,27 +1,27 @@
-CREATE DATABASE school;
+CREATE DATABASE escola;
 
-\c school; 
+\c escola; 
 
-CREATE TABLE classes (
+CREATE TABLE salas (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE students (
+CREATE TABLE alunos (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    class_id INTEGER REFERENCES classes(id) ON DELETE SET NULL,
+    sala_id INTEGER REFERENCES salas(id) ON DELETE SET NULL,
     photo TEXT
 );
 
-INSERT INTO classes (name) VALUES
-('Class A'),
-('Class B'),
-('Class C'),
-('Class D'),
-('Class E');
+INSERT INTO salas (name) VALUES
+('Sala A'),
+('Sala B'),
+('Sala C'),
+('Sala D'),
+('Sala E');
 
-INSERT INTO students (name, class_id, photo) VALUES
+INSERT INTO alunos (name, sala_id, photo) VALUES
 ('Bernardo', 1, NULL),
 ('Pedro', 2, NULL),
 ('Luiz', 3, NULL),
