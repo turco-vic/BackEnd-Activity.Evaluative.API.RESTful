f0c2133f4931116b3e66f859ad4ab66083d043ff
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-05-22 11:57:22 -0300

    Update schema.sql

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -33,3 +33,50 @@ INSERT INTO alunos (name, sala_id, idade, photo) VALUES
 ('Ana', 3, 13, NULL),
 ('Giovana', 4, 14, NULL),
 ('Mariana', 5, 15, NULL);
+
+INSERT INTO alunos (name, sala_id, idade, photo) VALUES
+('Kevin', 1, 11, NULL),
+('Gabriel', 2, 12, NULL),
+('Rafael', 3, 13, NULL),
+('Felipe', 4, 14, NULL),
+('Matheus', 5, 15, NULL),
+('Fernanda', 1, 11, NULL),
+('Larissa', 2, 12, NULL),
+('Juliana', 3, 13, NULL),
+('Tatiane', 4, 14, NULL),
+('Priscila', 5, 15, NULL),
+
+INSERT INTO alunos (name, sala_id, idade, photo) VALUES
+('Priscilo', 1, 11, NULL),
+('Jefferson', 2, 12, NULL),
+('Vinicius', 3, 13, NULL),
+('Eduardo', 4, 14, NULL),
+('Thiago', 5, 15, NULL),
+('Amanda', 1, 11, NULL),
+('Patricia', 2, 12, NULL),
+('Camila', 3, 13, NULL),
+('Roberta', 4, 14, NULL),
+('Sabrina', 5, 15, NULL);
+
+INSERT INTO salas (name) VALUES
+('Sala F6'),
+('Sala G7'),
+('Sala H8'),
+('Sala I9'),
+('Sala J10'),
+('Sala K11'),
+('Sala L12'),
+('Sala M13'),
+('Sala N14'),
+('Sala O15'),
+('Sala P16'),
+('Sala Q17'),
+('Sala R18'),
+('Sala S19'),
+('Sala T20'),
+('Sala U21'),
+('Sala V22'),
+('Sala W23'),
+('Sala X24'),
+('Sala Y25'),
+('Sala Z26');
